4d94f83a628df26e2b01f2b772917508641626ba
mohadeseh <mohadeseh73119@gmail.com>
2019-06-24 20:31:39 +0430

    Database

---
+++ database-shop/category.sql
@@ -0,0 +1,4 @@
+INSERT INTO `category` VALUES (1, 'Digital devices', NULL);
+INSERT INTO `category` VALUES (2, 'Fashion and clothing', NULL);
+INSERT INTO `category` VALUES (5, 'Makeup and hygiene', NULL);
+INSERT INTO `category` VALUES (7, 'mo', NULL);
---
+++ database-shop/product.sql
@@ -0,0 +1,5 @@
+INSERT INTO `product` VALUES (3, 'galaxy A7', NULL, NULL, NULL, NULL);
+INSERT INTO `product` VALUES (5, 'galaxy s7', NULL, NULL, NULL, NULL);
+INSERT INTO `product` VALUES (6, 'galaxy j5', NULL, NULL, NULL, NULL);
+INSERT INTO `product` VALUES (7, 'galaxy j5', NULL, NULL, NULL, NULL);
+INSERT INTO `product` VALUES (8, 'galaxy a3', NULL, NULL, NULL, NULL);
